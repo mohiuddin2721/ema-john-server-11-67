c5bbf33c7cd25bb2050fc84a2c2d71bc587f1135
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-28 00:55:35 +0600

    server are updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,17 @@ async function run() {
         app.get('/productCount', async (req, res) => {
             const count = await productsCollection.estimatedDocumentCount();
             res.send({ count });
+        })
+
+        // use post to get products by ids
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id))
+            const query = {_id: {$in: ids}}
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
         })
     }
     finally {
